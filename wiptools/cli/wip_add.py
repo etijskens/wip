4f84b3807f6fa271727932c3a7c263e408a2e0ee
Bert Tijskens <engelbert.tijskens@uantwerpen.be>
2023-06-23 09:58:39 +0200

    bug fix in relative path

--- wiptools/cli/wip_add.py
+++ wiptools/cli/wip_add.py
@@ -16,14 +16,12 @@ def wip_add(ctx: click.Context):
 
     cookiecutter_params = utils.read_wip_cookiecutter_json()
 
+    project_path = Path.cwd()
     package_name = cookiecutter_params['package_name']
-
-    package_path = Path.cwd() / package_name
-    submodule_path = package_path / ctx.params['submodule_path']
+    submodule_path = project_path / package_name / ctx.params['submodule_path']
     submodule_name = submodule_path.name
     parent_module = submodule_path.parent.name
-    parent_relpath = str(submodule_path.parent.relative_to(package_path))
-    parent_pypath = '' if (parent_relpath == '.') else (parent_relpath.replace(os.sep,'.') + '.')
+    parent_pypath = str(submodule_path.parent.relative_to(project_path)).replace(os.sep,'.') + '.'
 
     if not submodule_path.parent.is_dir():
         messages.error_message(f"The parent directory `{submodule_path.parent}` does not exist.")
@@ -46,7 +44,7 @@ def wip_add(ctx: click.Context):
                     )
 
     template = str(utils.cookiecutters() / 'submodule-py-tests')
-    output_dir = (project_path / 'tests' / parent_relpath)
+    output_dir = (project_path / 'tests' / submodule_path.parent.relative_to(project_path / package_name))
     with messages.TaskInfo(f"Expanding cookiecutter template `{template}`"):
         cookiecutter( template=template
                     , extra_context=cookiecutter_params
