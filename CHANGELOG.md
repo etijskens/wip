5875b0fc317504b31aef43ce8a59ba53930c3e96
Bert Tijskens <engelbert.tijskens@uantwerpen.be>
2023-07-04 17:31:29 +0200

    Bump version: 1.2.3-dev → 1.2.4-dev

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,5 @@
 **unreleased**
+**v1.2.4-dev**
 **v1.2.3-dev**
 **v1.2.2-dev**
 **v1.2.1-dev**
--- wiptools/__init__.py
+++ wiptools/__init__.py
@@ -6,7 +6,7 @@
 Common tools between the CLIs
 """
 
-__version__ = "1.2.3-dev"
+__version__ = "1.2.4-dev"
 
 def version():
     return f"wiptools v{__version__}"
